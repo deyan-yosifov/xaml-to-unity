137a372446b901cb5d7aa6653174b4bbd62180e4
deyan-yosifov <yosifov.deyan@gmail.com>
2020-02-05 22:16:35 +0200

    Implemented all UI components for the demo, except from the info button and its tooltip.

---
+++ Unity-CAGD-Demo/Assets/CAGD/Scripts/SampleScripts/SliderLabel.cs
@@ -0,0 +1,24 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace CAGD.SampleScripts
+{
+    public class SliderLabel : MonoBehaviour
+    {
+        [SerializeField]
+        private TextMeshProUGUI text;
+        [SerializeField]
+        private Slider slider;
+
+        private void Awake()
+        {
+            this.UpdateValue();
+        }
+
+        public void UpdateValue()
+        {
+            this.text.text = this.slider.value.ToString();
+        }
+    }
+}
