988395892e940c2ee9c4d79bb21a8e2f06294789
deyan-yosifov <yosifov.deyan@gmail.com>
2020-02-09 19:52:33 +0200

    Implemented SliderPropertyBinder. Now slider bindings are working perfectly. Need to implement checkbox bindings.

--- Unity-CAGD-Demo/Assets/CAGD/Scripts/SampleScripts/PropertyBinder.cs
+++ Unity-CAGD-Demo/Assets/CAGD/Scripts/SampleScripts/PropertyBinder.cs
@@ -3,40 +3,43 @@ using System.ComponentModel;
 using System.Reflection;
 using UnityEngine;
 
-public abstract class PropertyBinder : MonoBehaviour
+namespace CAGD.SampleScripts
 {
-    [SerializeField]
-    private string propertyName;
-    private INotifyPropertyChanged viewModel;
-    private PropertyInfo property;
-
-    protected abstract void GetPropertyValue(object value);
-
-    protected void SetPropertyValue(object value)
+    public abstract class PropertyBinder : MonoBehaviour
     {
-        this.property.SetValue(this.viewModel, value);
-    }
+        [SerializeField]
+        private string propertyName = null;
+        private INotifyPropertyChanged viewModel;
+        private PropertyInfo property;
 
-    private void Awake()
-    {
-        this.viewModel = this.GetComponentInParent<INotifyPropertyChanged>();
-        this.viewModel.PropertyChanged += this.ViewModelPropertyChanged;
-        Type viewModelType = this.viewModel.GetType();
-        this.property = viewModelType.GetProperty(this.propertyName);
-        this.GetPropertyValue();
-    }
+        protected abstract void GetPropertyValue(object value);
 
-    private void ViewModelPropertyChanged(object sender, PropertyChangedEventArgs e)
-    {
-        if (e.PropertyName == this.propertyName)
+        protected void SetPropertyValue(object value)
         {
+            this.property.SetValue(this.viewModel, value);
+        }
+
+        protected virtual void Awake()
+        {
+            this.viewModel = this.GetComponentInParent<INotifyPropertyChanged>();
+            this.viewModel.PropertyChanged += this.ViewModelPropertyChanged;
+            Type viewModelType = this.viewModel.GetType();
+            this.property = viewModelType.GetProperty(this.propertyName);
             this.GetPropertyValue();
         }
-    }
 
-    private void GetPropertyValue()
-    {
-        object value = this.property.GetValue(this.viewModel);
-        this.GetPropertyValue(value);
+        private void ViewModelPropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            if (e.PropertyName == this.propertyName)
+            {
+                this.GetPropertyValue();
+            }
+        }
+
+        private void GetPropertyValue()
+        {
+            object value = this.property.GetValue(this.viewModel);
+            this.GetPropertyValue(value);
+        }
     }
 }
---
+++ Unity-CAGD-Demo/Assets/CAGD/Scripts/SampleScripts/SliderPropertyBinder.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+namespace CAGD.SampleScripts
+{
+    [RequireComponent(typeof(Slider))]
+    public class SliderPropertyBinder : PropertyBinder
+    {
+        private Slider slider;
+
+        protected override void Awake()
+        {
+            this.slider = this.GetComponent<Slider>();
+            this.slider.onValueChanged.AddListener(new UnityAction<float>(this.OnSliderValueChanged));
+
+            base.Awake();
+        }
+
+        protected override void GetPropertyValue(object value)
+        {
+            float v;
+
+            if (this.slider.wholeNumbers)
+            {
+                v = (int)value;
+            }
+            else
+            {
+                v = (float)value;
+            }
+             
+            this.slider.value = v;
+        }
+
+        private void OnSliderValueChanged(float value)
+        {
+            object v;
+
+            if (slider.wholeNumbers)
+            {
+                v = (int)value;
+            }
+            else
+            {
+                v = value;
+            }
+
+            this.SetPropertyValue(v);
+        }
+    }
+}
